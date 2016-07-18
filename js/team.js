c0aca6cf85d0ad84edc1451b32a4f08f0340ece1
valeserber <valserber@gmail.com>
2016-07-17 21:56:44 -0300

    separe teams

---
+++ js/team.js
@@ -0,0 +1,15 @@
+$('#click_to_slide_2016').click(function () {
+        $('#content_down_2016').slideToggle('slow');
+    });
+$('#click_to_slide_2015').click(function () {
+        $('#content_down_2015').slideToggle('slow');
+    });
+$('#click_to_slide_2014').click(function () {
+        $('#content_down_2014').slideToggle('slow');
+    });
+$('#click_to_slide_visits_2015').click(function () {
+        $('#content_down_visits_2015').slideToggle('slow');
+    });
+$('#click_to_slide_visits_2014').click(function () {
+        $('#content_down_visits_2014').slideToggle('slow');
+    });
